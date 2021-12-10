27b479b6dd29a6edd430c7c22bcb28d8f68f00db
leone <leone-duarte@outlook.com>
2021-12-10 13:51:30 -0300

    Brincando react

--- src/index.jsx
+++ src/index.jsx
@@ -4,10 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Leone',
+  lastName: 'Duarte'
+};
+
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
+);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  element,
   document.getElementById('root')
 );
 
